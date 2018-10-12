53a27cff8385a0159cdeeefc055c192fded7bf19
nicholasyyu <nicholas.yu24@gmail.com>
2018-10-12 02:12:36 +0000

    feature/users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+	def new
+    	@user = User.new
+	end
+
+	def create
+	
+    	@user = User.new(user_params)
+    	if @user.save!
+    		session[:user_id] = @user.id
+	      	redirect_to '/', notice: 'User created!'
+	    else
+	      	render :new
+	    end
+  	end
+
+	private
+	def user_params
+		params.require(:user).permit(
+			:last_name,
+      		:first_name,
+      		:email,
+      		:password,
+      		:password_confirmation
+    	)
+  	end
+end
