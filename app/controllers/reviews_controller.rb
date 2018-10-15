e34d867d12e76dd42ef6fbe06d6f5f2c6a2c44b2
nicholasyyu <nicholas.yu24@gmail.com>
2018-10-15 00:16:32 +0000

    login user can write to product

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,23 @@
+class ReviewsController < ApplicationController
+
+	def create
+    	@product = Product.find(params[:product_id])
+    	@review = Review.new(review_params)
+
+    	@review.user_id = current_user.id
+    	@review.product_id = @product.id
+
+    	if @review.save
+    		redirect_to product_path(@product)
+    	else
+    		render :'products/show'
+    	end
+
+    end
+
+    private
+    def review_params
+    	params.require(:review).permit(:description, :rating)
+    end
+
+end
